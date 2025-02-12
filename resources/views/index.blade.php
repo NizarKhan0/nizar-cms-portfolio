f207fca364eddd787fd19285b28a58dca1401e40
Nizar Khan <131904091+NizarKhan0@users.noreply.github.com>
2025-02-12 22:55:28 +0800

    add google analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,16 @@
 {{-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth"> --}}
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CBN627L0J6"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-CBN627L0J6');
+    </script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
